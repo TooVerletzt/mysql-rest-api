a9f19988ec02ea291f0c3690af3563f38c66fb34
GLOCK <147098431+TooVerletzt@users.noreply.github.com>
2025-04-22 01:24:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
-
 const server = require('./server');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`Server is live at localhost:${PORT}`));
+server.listen(PORT, ()=> console.log('Server is live at localhost :' + PORT));
 

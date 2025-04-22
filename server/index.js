f8e3cbc97e563a23dab7d88e8d740a7a4f6826e0
GLOCK <147098431+TooVerletzt@users.noreply.github.com>
2025-04-22 01:23:22 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const cors = require('cors');
 const express = require('express');
-const { sequelize } = require('../models');  // Asegúrate de importar sequelize
 const routes = require('../routes');
 
 const server = express();
@@ -9,16 +8,4 @@ server.options('*', cors());
 
 server.use(express.json());
 server.use('/api', routes);
-
-sequelize.sync({ force: true }) // Esto reinicia las tablas
-  .then(() => {
-    console.log('✅ Tablas sincronizadas con la base de datos');
-    server.listen(process.env.PORT || 3000, () => {
-      console.log(`Servidor corriendo en http://localhost:${process.env.PORT || 3000}`);
-    });
-  })
-  .catch((err) => {
-    console.error('❌ Error al sincronizar tablas:', err.message);
-  });
-
-
+module.exports = server;
